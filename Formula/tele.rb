9d3f770d8c7d1002f2e7526970234cf37a5ff4a6
Arda Sevinç <arda@ardasevinc.com>
2026-07-15 05:53:59 +0300

    chore: update tele to alpha.11

--- Formula/tele.rb
+++ Formula/tele.rb
@@ -5,21 +5,21 @@ class Tele < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.10/tele_0.1.0-alpha.10_darwin_arm64.tar.gz"
-      sha256 "0c02f985ac42d945765d9e68d7e1db9392ac80c6353e75aab7a54db5f6f7ae21"
+      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_darwin_arm64.tar.gz"
+      sha256 "dcb2098450bd1e2b721d212344adaacaadfeaa62e10c9d385e881676267f2baa"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.10/tele_0.1.0-alpha.10_darwin_amd64.tar.gz"
-      sha256 "1e2000ef5b414f338bcc8a050cc63d085bb7c30ed81eaaaac3e3b16362be4a4d"
+      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_darwin_amd64.tar.gz"
+      sha256 "85940d129a05d9cdc087c67e444141aad253ceb734f074827b3c7f1f690e3837"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.10/tele_0.1.0-alpha.10_linux_arm64.tar.gz"
-      sha256 "38c532c575bf0a24e139e0ffa7a5da8cc7c5ddcf4b893f2fb6582d9c30fa81c3"
+      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_linux_arm64.tar.gz"
+      sha256 "9979f8720712c401a2178a5a7527d5d339fc05d7cbf52d1495ee17a08f86a6aa"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.10/tele_0.1.0-alpha.10_linux_amd64.tar.gz"
-      sha256 "10f44155a85140ef4fd1fdc1cd7a096b1e3bc268db1ce4c0815c6ad533ba41a9"
+      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_linux_amd64.tar.gz"
+      sha256 "e0788c3a354969a2e590554ce2a1afb455c29244318a3b07417c0599205aa012"
     end
   end
 
