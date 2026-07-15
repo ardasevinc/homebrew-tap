4147f197c4a3ac6c71c2aacb7318dca0136d9a84
Arda Sevinç <arda@ardasevinc.com>
2026-07-15 06:39:27 +0300

    chore: update tele to v1.0.1

--- Formula/tele.rb
+++ Formula/tele.rb
@@ -5,30 +5,32 @@ class Tele < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_darwin_arm64.tar.gz"
-      sha256 "dcb2098450bd1e2b721d212344adaacaadfeaa62e10c9d385e881676267f2baa"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.0.1/tele_1.0.1_darwin_arm64.tar.gz"
+      sha256 "50f575e6907843d879d2c2d6496882503b4fd4f013b5c3f1244924ab59d4e4b8"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_darwin_amd64.tar.gz"
-      sha256 "85940d129a05d9cdc087c67e444141aad253ceb734f074827b3c7f1f690e3837"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.0.1/tele_1.0.1_darwin_amd64.tar.gz"
+      sha256 "18fa01a005dd1d41596b6a2ac27bfe517879f4df7cf40fc6a7408ca14cd57092"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_linux_arm64.tar.gz"
-      sha256 "9979f8720712c401a2178a5a7527d5d339fc05d7cbf52d1495ee17a08f86a6aa"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.0.1/tele_1.0.1_linux_arm64.tar.gz"
+      sha256 "8d2e6e648d2664f8d89f5ca59256f597d818f2dbc0b192944b202bfbe2032e8a"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v0.1.0-alpha.11/tele_0.1.0-alpha.11_linux_amd64.tar.gz"
-      sha256 "e0788c3a354969a2e590554ce2a1afb455c29244318a3b07417c0599205aa012"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.0.1/tele_1.0.1_linux_amd64.tar.gz"
+      sha256 "da157d581ae95d71b5459c857576e210da427417436ffabf3521c1a2a9a8c493"
     end
   end
 
   def install
     bin.install "tele"
+    pkgshare.install "LICENSE"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/tele --version")
     assert_match "Usage:", shell_output("#{bin}/tele --help")
+    assert_path_exists pkgshare/"LICENSE"
   end
 end
