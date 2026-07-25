1f1d8f8bd990ea9d3980bff1e3fc9dbeefc52566
Arda Sevinç <arda@ardasevinc.com>
2026-07-25 18:48:52 +0300

    chore: update tele to v1.1.0

--- Formula/tele.rb
+++ Formula/tele.rb
@@ -1,26 +1,26 @@
 class Tele < Formula
   desc "Unofficial Telegram MTProto CLI for agents and humans"
   homepage "https://github.com/ardasevinc/tele"
-  version "1.0.2"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v1.0.2/tele_1.0.2_darwin_arm64.tar.gz"
-      sha256 "76f03cb5a7bdef8815944c2a5514fb2423cb477bb7aac323007f9e643b4ee8d8"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.1.0/tele_1.1.0_darwin_arm64.tar.gz"
+      sha256 "b9073319e11ffff83bc59003b83d9603195bc21cc30945d0ad9e3da76ae50fb6"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v1.0.2/tele_1.0.2_darwin_amd64.tar.gz"
-      sha256 "908735cfeb61dc6d87177dd9066ca57eb421b40e0090f78c10274a831f180840"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.1.0/tele_1.1.0_darwin_amd64.tar.gz"
+      sha256 "94bacd96f7f25fcca27efc23ac8106a38fad70414d5379b8778ce26cd55064d0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ardasevinc/tele/releases/download/v1.0.2/tele_1.0.2_linux_arm64.tar.gz"
-      sha256 "b27dba22163ae8b61e950c6059dc42e3e6fe98d3a662e941dca3c8c54f1caace"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.1.0/tele_1.1.0_linux_arm64.tar.gz"
+      sha256 "ac687dbe6dbcf7eb651ffe6927a2d9aa8e6e3adf86e538e6b3108569150a33d7"
     else
-      url "https://github.com/ardasevinc/tele/releases/download/v1.0.2/tele_1.0.2_linux_amd64.tar.gz"
-      sha256 "17f3add0f6c2067bff03c6e93bcf96bd9f3ccfe17d2b73c84a4ff3b836eb1e31"
+      url "https://github.com/ardasevinc/tele/releases/download/v1.1.0/tele_1.1.0_linux_amd64.tar.gz"
+      sha256 "3bcb3b1c99333eb53fcc1127eca483e20531214e7a78e50c1e61043ffa588e9e"
     end
   end
 
