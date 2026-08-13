6479bec0de9d01822e63e79819f7c507e22a3dc0
Arda Sevinç <arda@ardasevinc.com>
2026-08-13 19:10:54 +0300

    chore: update mattermost-cli to v2.1.0

--- Formula/mattermost-cli.rb
+++ Formula/mattermost-cli.rb
@@ -1,26 +1,26 @@
 class MattermostCli < Formula
   desc "Mattermost CLI for agents and humans"
   homepage "https://github.com/ardasevinc/mattermost-cli"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ardasevinc/mattermost-cli/releases/download/v#{version}/mattermost-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "4d2e1c2841bd9f573ae4751d3c68daa4e7620ad228dbe21841cd85ed88bc9361"
+      sha256 "a1e21d40cb8bc9e718f05ea2541cde5f83ca4cfd4aba3d406e16e84dd3aa8110"
     else
       url "https://github.com/ardasevinc/mattermost-cli/releases/download/v#{version}/mattermost-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "f1be5b05a53d21514ff89bd0296139fe104fc16188f59c5f7f19b7bfa2829793"
+      sha256 "edf9d707d8dc3bdea1fd526f31a714d386d7d6cb4f18c6fd581e6a61cf1d9c65"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ardasevinc/mattermost-cli/releases/download/v#{version}/mattermost-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "aeed44ed5511d49f87c8c6b7068185891c037380c892e55bb62972b34487a02b"
+      sha256 "d61e08a0ae74c089a4238ab38416d011b71aebc4233f31fa3de340354fba529b"
     else
       url "https://github.com/ardasevinc/mattermost-cli/releases/download/v#{version}/mattermost-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "01ea79cb117425eb0127ea9791981f62d6006a8921a91b5c293defe1488fd6c0"
+      sha256 "70cf07e2d6acf60af85b12729df4782a133f61a72f2471e992e382b114164061"
     end
   end
 
