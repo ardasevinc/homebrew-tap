a8c5e9ec1267dd8970d44c35a5bc2337101552ea
Arda Sevinç <arda@ardasevinc.com>
2026-09-06 01:36:56 +0300

    fix: migrate bridge cask to declarative install steps

--- Casks/herdr-codex-bridge.rb
+++ Casks/herdr-codex-bridge.rb
@@ -33,9 +33,9 @@ cask "herdr-codex-bridge" do
 
   binary "herdr-self"
 
-  postflight do
-    if OS.mac?
-      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/herdr-self"]
+  postflight_steps do
+    on_macos do
+      run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "herdr-self"], chdir: "."
     end
   end
 
